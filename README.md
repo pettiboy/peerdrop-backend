f9e5318d638a9f4f1b0307ce449155c35c05722f
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2025-03-06 00:57:49 +0530

    feat: simple json req res using actix web

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Peerdrop Backend
+
+For testing
+
+```zsh
+watchexec -e rs -r cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{ web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+use serde_json::json;
+
+#[derive(Deserialize)]
+struct Info {
+    username: String,
+}
+
+async fn index(info: web::Json<Info>) -> impl Responder {
+    HttpResponse::Ok().json(json!({
+        "message": format!("Welcome {}!", info.username),
+    }))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(web::resource("/").route(web::post().to(index)))
+    })
+    .bind(("127.0.0.1", 9081))?
+    .run()
+    .await
 }
